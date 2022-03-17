a20158c5bd58342d04435115c413cb28cff92294
Iqrima <iqrimannisa@gmail.com>
2022-03-17 11:30:54 +0700

    riry mengubah

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,13 @@
+<footer>
+	<p>Copyright &copy; 2022 Beauty Shop</p>
+		<div class="social right">
+			<a href="#"><i class="fa fa-facebook"></i></a>
+			<a href="#"><i class="fa fa-twitter"></i></a>
+			<a href="#"><i class="fa fa-google-plus"></i></a>
+			<a href="#"><i class="fa fa-dribbble"></i></a>
+			<a href="#"><i class="fa fa-instagram"></i></a>
+			<a href="#"><i class="fa fa-linkedin"></i></a>
+		</div>
+</footer>
+</body>
+</html>
