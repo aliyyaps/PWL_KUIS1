5f59132173665d1e7f81bfe58b81869fd92215c4
Iqrima <iqrimannisa@gmail.com>
2022-03-17 10:13:20 +0700

    iqrima mengubah

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!-- templatemo 413 flip turn -->
+<!-- 
+Flip Turn Template 
+http://www.templatemo.com/tm-413-flip-turn
+-->
+<head>
+	<title>Flip Turn Template</title>
+	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+	<link href="asset/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="asset/css/magnific-popup.css" rel="stylesheet"> 
+	<link href="asset/css/templatemo_style.css" rel="stylesheet" type="text/css">	
+</head>
+<body>
+	<div class="main-container">
+		<nav class="main-nav">
+			<div id="logo" class="left"><a href="#">Flip Turn</a></div>
+			<ul class="nav right center-text">
+				<li class="btn">Home</li>
+				<li class="btn"><a href="about.html">About</a></li>
+				<li class="btn"><a href="awards.html">Awards</a></li>				
+				<li class="btn"><a href="contact.html">Contact</a></li>
+				<li class="btn"><a rel="nofollow" href="http://www.facebook.com/templatemo" target="_parent">External</a></li>
+			</ul>
+		</nav>
